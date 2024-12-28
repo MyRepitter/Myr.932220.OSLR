35ee27b765868dc75a796dc1224d0d998642cd66
Myraviev <myrpeter24@gmail.com>
2024-12-28 10:52:31 +0700

    Update LR_2.c

--- LR_2.c
+++ LR_2.c
@@ -8,48 +8,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-volatile sig_atomic_t wasSigHup = 0;         // sig_atomic_t - гарантирует, что компилятор операцию с этим типом данных всегда будет представлять в виде одной инструкции
-void sigHupHandler(int r) { wasSigHup = 1; } // Объявление безопасного обработчика сигнала обработчика сигнала SigHup
+volatile sig_atomic_t wasSigHup = 0;           // sig_atomic_t - гарантирует, что компилятор операцию с этим типом данных всегда будет представлять в виде одной инструкции
+void sigHupHandler(int r) { wasSigHup = 1; }   // Объявление безопасного обработчика сигнала SigHup
 
 int main() {
 	// Регистрация обработчика сигнала
 	struct sigaction sa;              // Заводим переменную типа sigaction
 	sigaction(SIGHUP, NULL, &sa);     // В эту переменную сбрасываем атрибуты этого сигнала для вашего процессора из ядра операционной системы
-	sa.sa_handler = sigHupHandler;    // Хранитт указатель на вашу функцию, в которой скрывается обработчик сигнала
+	sa.sa_handler = sigHupHandler;    // Хранит указатель на нашу функцию, в которой скрывается обработчик сигнала
 	sa.sa_flags |= SA_RESTART;        // Устанавливаем флаг
-	sigaction(SIGHUP, &sa, NULL);     // Повторный вызов функции sigaction уже с просьбой вашу модифицированную структуру сохранить в ядре
+	sigaction(SIGHUP, &sa, NULL);     // Повторный вызов функции sigaction уже с просьбой нашу модифицированную структуру сохранить в ядре
 	// Выгружаем из ядра то, что было на текущий момент, модифицируем, затем возвращаем в ядро уже в модифицированном виде
 
 	// Блокировка сигнала
 	sigset_t blockedMask, origMask;                    // Заводим маску блокирования сигналов и исходную маску сигналов - маску, с которой наше приложение было запущено, она подразумевает что сигнал SigHup еще был разблокирован
-	                                                   //     она идентифицирует те сигналы, которые должны быть активированы на время исполнения Pselect
+	                                                   //     она идентифицирует те сигналы, которые должны быть активированы на время исполнения pselect()
 	sigemptyset(&blockedMask);                         // Сбрасываем маску blockedMask
 	sigaddset(&blockedMask, SIGHUP);                   // Помечаем нужный сигнал, в вашем случае это SIGHUP
 	sigprocmask(SIG_BLOCK, &blockedMask, &origMask);   // Говорим, что необходимо заблокировать те сигналы, которые присутствуют в вашей множестве
 
 	int serverSocket = socket(AF_INET, SOCK_STREAM, 0); // Создание серверного сокета (его дескриптора)
 
-	struct sockaddr_in serverAddr;
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serverAddr.sin_port = htons(2048);
+	// Создаём и настраиваем структуру sockaddr_in для работы с интернет-протоколом (IPv4)
+	struct sockaddr_in serverAddress;                      // Заводим переменную типа sigaction
+	serverAddress.sin_family = AF_INET;                    // Указываем что используем IPv4 адресацию
+	serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);     // Задаём адрес к которому будет привязан сокет, INADDR_ANY позволяет сокету принимать соединения с любого IP-адреса, а htonl преобразует этот адрес из формата хоста в сетевой формат (big-endian)
+	serverAddress.sin_port = htons(5555);                  // Устанавливаем номер порта, на котором будет слушать сокет
 
-	bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
-	listen(serverSocket, SOMAXCONN);
+	bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));   // Связываем сокет с адресом, указанным в serverAddress, чтобы операционная система знала, на каком IP-адресе и порту будет слушать ваш сервер
+	listen(serverSocket, SOMAXCONN);                                               // Переводим сокет в состояние прослушивания 
 
-	printf("Server is running\n");
+	printf("Server is running\n");  // Оповещаем о запуске сервера
 
-	int clientSocket; // Объявление клиентского сокета (его дескриптора)
-	int maxFd = serverSocket; // Переменная наибольшего дескриптора сокета, для мониторинга входящих подключений
-	fd_set fds; // Заводим множество файловых дескрипторов
-	bool isConnected = false; 
+	int clientSocket;                   // Объявление клиентского сокета (его дескриптора)
+	int maxFd = serverSocket;           // Переменная наибольшего дескриптора сокета, для мониторинга входящих подключений
+	fd_set fds;                         // Заводим множество файловых дескрипторов
+	bool isConnected = false;           // Ставим isConnected на фолс, так как ещё нет соединения
 
 	while (true)
 	{
 		FD_ZERO(&fds); // Сбрасываем множество файловых дескрипторов
 		
-		FD_SET(serverSocket, &fds); // Добавляем серверный сокет во множество файловых дескрипторов
-		maxFd = serverSocket; // Сохраняем значение серверного сокета в maxFd
+		FD_SET(serverSocket, &fds);   // Добавляем серверный сокет во множество файловых дескрипторов
+		maxFd = serverSocket;         // Сохраняем значение серверного сокета в maxFd
 
 		FD_SET(clientSocket, &fds); // Добавляем клиентский сокет во множество файловых дескрипторов
 		
@@ -61,8 +62,8 @@ int main() {
 			{
 				if (wasSigHup) // Проверяем был ли это сигнал SigHub
 				{
-					wasSigHup = 0; // Условно обрабатываем сигнал SigHup
-					printf("SIGHUP was received\n"); // Оповещаем о получениии сигнала SigHup
+					wasSigHup = 0;                     // Условно обрабатываем сигнал SigHup
+					printf("SIGHUP was received\n");   // Оповещаем о получениии сигнала SigHup
 				}
 				continue; // Переходим к следующей итеррации цикла, так как ошибка типа EINTER для нас по факту не является ошибкой
 			}
@@ -73,7 +74,7 @@ int main() {
 		}
 
 		// Логика приёма нового соединения
-		if (FD_ISSET(serverSocket, &fds))  // Используем макрос FD_ISSET для проверки, готов ли серверный сокет к обработке нового соединения
+		if (FD_ISSET(serverSocket, &fds))  // Используем FD_ISSET для проверки, готов ли серверный сокет к обработке нового соединения
                 {
 			int clientSocketNEW = accept(serverSocket, NULL, NULL); // Заводим новый сокет и присваеваем значение функции accept(), которая принимает входящее соединение, создавая новый сокет для общения с новым клиентом
 			if (clientSocketNEW != -1) // Если новое соединение есть
@@ -85,16 +86,16 @@ int main() {
 					printf("Close: %d\n", clientSocket);
 					close(clientSocket);
 				}
-				isConnected = true; // Есть подключенный клиент
-				clientSocket = clientSocketNEW; // В переменную клиентского сокета сохраняем новое соединение
+				isConnected = true;               // Есть подключенный клиент
+				clientSocket = clientSocketNEW;   // В переменную клиентского сокета сохраняем новое соединение
 			}
 		}
 
                 // Логика приёма данных
-		if (FD_ISSET(clientSocket, &fds)) // Используем макрос FD_ISSET для проверки, есть ли данные для чтения из клиентского сокета
+		if (FD_ISSET(clientSocket, &fds)) // Используем FD_ISSET для проверки, есть ли данные для чтения из клиентского сокета
 		{
-			char buffer[1024]; // Устанавливаем значение того, сколько нужно будет прочесть функции read()
-			ssize_t readVolume = read(clientSocket, buffer, sizeof(buffer)); // Присваиваем переменной readVolume количество байтов которое удалось проситать функции read() 
+			char buffer[1024];                                                       // Устанавливаем значение того, сколько нужно будет прочесть функции read()
+			ssize_t readVolume = read(clientSocket, buffer, sizeof(buffer));         // Присваиваем переменной readVolume количество байтов которое удалось проситать функции read() 
 			
 			if (readVolume <= 0) // Если функции read() не удалось ни чего прочитать, закрываем клиентский сокет с оповещением
 			{
@@ -102,7 +103,7 @@ int main() {
 				close(clientSocket);
 				isConnected = false;
 			}
-			else printf("%zd bits were read from %d\n", readVolume, clientSocket); // Оповещаем о том, сколько байтов информации прочла функция read() из текущего склиентского сокета
+			else printf("%zd bits were read from %d\n", readVolume, clientSocket); // Оповещаем о том, сколько байтов информации прочла функция read() из текущего clientSocket
 		}
 	}
 
